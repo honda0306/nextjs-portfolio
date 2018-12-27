2a7f011be4e3e19691192480e5df7185dce049ec
Chris Honda <christopher.honda92@gmail.com>
2018-12-27 12:13:47 -0800

    Initial commit; initialized with project dependencies

---
+++ .next/static/chunks/0.js
@@ -0,0 +1,16 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[0],{
+
+/***/ "./node_modules/next/dist/client/noop.js":
+/*!***************************************************************************************************************!*\
+  !*** C:/Users/chris/OneDrive/Desktop/PersonalProjects/nextjs-portfolio/node_modules/next/dist/client/noop.js ***!
+  \***************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=0.js.map
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+const Index = () => {
+    return (
+        <h1>I am the Index page.</h1>
+    )
+}
+
+export default Index;
